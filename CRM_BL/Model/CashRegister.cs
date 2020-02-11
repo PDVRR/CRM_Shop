898bfdc574afb6494d5dfd720caee3262a789e26
Tr1oniX <tr1onix2k@gmail.com>
2020-02-11 18:13:48 +0300

    Добавлена модель кассы, реализован кассовый аппарат

---
+++ CRM_BL/Model/CashRegister.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_BL.Model
+{
+    public class CashRegister
+    {
+        public int Number { get; set; }
+        public Seller Seller { get; set; }
+        public Queue<Cart> Queue { get; set; }
+        public int MaxQueueLength { get; set; }
+        public int GoneCustomer { get; set; }
+        public bool IsModel { get; set; }
+
+        CrmContext db = new CrmContext();
+
+        public CashRegister(int number, Seller seller)
+        {
+            Number = number;
+            Seller = seller;
+            Queue = new Queue<Cart>();
+            IsModel = true;
+
+        }
+
+        public void Enqueue(Cart cart)
+        {
+            if (Queue.Count <= MaxQueueLength)
+            {
+                Queue.Enqueue(cart);
+            }
+            else
+            {
+                GoneCustomer++;
+            }
+        }
+
+        public decimal Dequeue()
+        {
+            decimal sum = 0;
+            var cart = Queue.Dequeue();
+
+            if (cart != null)
+            {
+                var check = new Check()
+                {
+                    SellerId = Seller.Id,
+                    CustomerId = cart.Customer.Id,
+                    Seller = Seller,
+                    Customer = cart.Customer,
+                    Created = DateTime.Now
+
+                };
+
+                if (!IsModel)
+                {
+                    db.Checks.Add(check);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    check.Id = 0;
+                }
+
+                var sells = new List<Sell>();
+
+                foreach (Product product in cart)
+                {
+                    if (product.Count > 0)
+                    {
+
+
+                        var sell = new Sell()
+                        {
+                            CheckId = check.Id,
+                            Check = check,
+                            ProductId = product.Id,
+                            Product = product
+                        };
+                        sells.Add(sell);
+
+                        if (!IsModel)
+                        {
+                            db.Sells.Add(sell);
+                        }
+
+                        product.Count--;
+                        sum += product.Price;
+                    }
+                }
+
+                if (!IsModel)
+                {
+                    db.SaveChanges();
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ CRM_BLTests/Model/CashRegisterTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CRM_BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_BL.Model.Tests
+{
+    [TestClass()]
+    public class CashRegisterTests
+    {
+        [TestMethod()]
+        public void CashRegisterTest()
+        {
+            var customer1 = new Customer()
+            {
+                Id = 1,
+                Name = "testuser1"
+            };
+            var customer2 = new Customer()
+            {
+                Id = 1,
+                Name = "testuser2"
+            };
+            var seller = new Seller()
+            {
+                Id = 1,
+                Name = "testseller"
+            };
+            var product1 = new Product()
+            {
+                Id = 1,
+                Name = "product1",
+                Price = 100,
+                Count = 2
+            };
+            var product2 = new Product()
+            {
+                Id = 2,
+                Name = "product2",
+                Price = 30,
+                Count = 6
+            };
+            var product3 = new Product()
+            {
+                Id = 3,
+                Name = "product3",
+                Price = 50,
+                Count = 5
+            };
+            var cart1 = new Cart(customer1);
+            cart1.Add(product1);
+            cart1.Add(product2);
+            cart1.Add(product3);
+            var cart2 = new Cart(customer2);
+            cart2.Add(product2);
+            cart2.Add(product2);
+            cart2.Add(product3);
+
+            var cashRegister = new CashRegister(1, seller)
+            {
+                MaxQueueLength = 3
+            };
+            cashRegister.Enqueue(cart1);
+            cashRegister.Enqueue(cart2);
+
+            var cart1ExpectedResult = 180;
+            var cart2ExpectedResult = 110;
+
+            var cart1ActualResult = cashRegister.Dequeue();
+            var cart2ActualResult = cashRegister.Dequeue();
+
+            Assert.AreEqual(cart1ExpectedResult, cart1ActualResult);
+            Assert.AreEqual(cart2ExpectedResult, cart2ActualResult);
+            Assert.AreEqual(1, product1.Count);
+            Assert.AreEqual(3, product2.Count);
+            Assert.AreEqual(3, product3.Count);
+        }
+
+        
+    }
+}
