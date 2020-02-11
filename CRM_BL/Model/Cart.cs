155e756a0701d29294882e739b2282d0c38e002b
Tr1oniX <tr1onix2k@gmail.com>
2020-02-11 17:14:23 +0300

    Реализована корзина

---
+++ CRM_BL/Model/Cart.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace CRM_BL.Model
+{
+    public class Cart : IEnumerable
+    {
+        public Customer Customer { get; set; }
+        public Dictionary<Product, int> Products { get; set; }
+        public int Count { get; private set; }
+
+        public Cart(Customer customer)
+        {
+            Customer = customer;
+            Products = new Dictionary<Product, int>();
+        }
+
+        public void Add(Product product)
+        {
+            if (Products.TryGetValue(product, out int count))
+            {
+                Products[product] = ++count;
+            }
+            else
+            {
+                Products.Add(product, 1);
+            }
+
+            Count++;
+        }
+
+        public List<Product> GetAll()
+        {
+            var result = new List<Product>();
+            foreach (Product product in this)
+            {
+                result.Add(product);
+            }
+
+            return result;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var product in Products.Keys)
+            {
+                for (int i = 0; i < Products[product]; i++)
+                {
+                    yield return product;
+                }
+            }
+        }
+
+    }
+}
---
+++ CRM_BLTests/Model/CartTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CRM_BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_BL.Model.Tests
+{
+    [TestClass()]
+    public class CartTests
+    {
+        [TestMethod()]
+        public void CartTest()
+        {
+            var customer = new Customer()
+            {
+                Id = 1,
+                Name = "testuser"
+            };
+            var product1 = new Product()
+            {
+                Id = 1,
+                Name = "product1",
+                Price = 100,
+                Count = 2
+            };
+            var product2 = new Product()
+            {
+                Id = 2,
+                Name = "product2",
+                Price = 100,
+                Count = 2
+            };
+            var product3 = new Product()
+            {
+                Id = 3,
+                Name = "product3",
+                Price = 100,
+                Count = 2
+            };
+            var cart = new Cart(customer);
+            cart.Add(product1);
+            cart.Add(product2);
+            cart.Add(product3);
+            var expectedResult = new List<Product>()
+            {
+                product1, product2, product3
+            };
+            var cartResult = cart.GetAll();
+            Assert.AreEqual(expectedResult.Count, cart.Count);
+            for (int i = 0; i < expectedResult.Count; i++)
+            {
+                Assert.AreEqual(expectedResult[i], cartResult[i]);
+            }
+        }
+
+    }
+}
